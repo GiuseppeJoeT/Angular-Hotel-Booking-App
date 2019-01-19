26955f9a756f02c776194e069043d06ed07f6854
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-19 15:06:42 +0100

    login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SearchComponent } from './features/search/search.component';
+
+const routes: Routes = [
+  { path: 'search', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
