bce57a40781aa43f9af1bfa8ae5d1665ef44fa29
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-19 15:07:47 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './features/search/search.component';
+import { LoginComponent } from './features/login/login.component';
+import { CartComponent } from './features/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    LoginComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
