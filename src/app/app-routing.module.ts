30c39979f442c8a59efb6fc3cdc739ad48087a24
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-01-20 15:39:08 +0100

    Update app-routing.module.ts
    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SearchComponent } from './features/search/search.component';
+import { LoginComponent } from './features/login/login.component';
+import { CartComponent } from './features/cart/cart.component';
 
 const routes: Routes = [
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
