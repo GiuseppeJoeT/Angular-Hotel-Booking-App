6256182093b80f5e812e382af94c098917485590
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-06 20:41:14 +0100

    root component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-Hotel-Booking-App'`, () => {
+  it(`should have as title 'hotel-booking'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular-Hotel-Booking-App');
+    expect(app.title).toEqual('hotel-booking');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Hotel-Booking-App!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hotel-booking!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Hotel-Booking-App';
+  // title = 'hotel-booking';
+  constructor(public auth: AuthService) {}
 }
