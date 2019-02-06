8b54b68a75fbdd4a2e3737f3d9f7852196350e7d
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-06 20:41:22 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { SearchComponent } from './features/search/search.component';
 import { LoginComponent } from './features/login/login.component';
@@ -16,7 +18,9 @@ import { CartComponent } from './features/cart/cart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
