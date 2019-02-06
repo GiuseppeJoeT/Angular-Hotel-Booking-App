2e59d2706526c838807699f30d25533a1e9f2e69
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-06 20:40:04 +0100

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { SearchComponent } from './features/search/search.component';
 import { LoginComponent } from './features/login/login.component';
 import { CartComponent } from './features/cart/cart.component';
@@ -8,7 +7,8 @@ import { CartComponent } from './features/cart/cart.component';
 const routes: Routes = [
   { path: 'search', component: SearchComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: 'search'}
 ];
 
 @NgModule({
