fff3a2e169fbaf3b4353504d68f132d948993b24
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-07 18:56:46 +0100

    search component

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Hotel } from './../../model/hotel';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  text = 'Milan';
+  // risultato search
+  hotels: Hotel[];
 
-  constructor() { }
+  constructor(private http: HttpClient ) {
+    this.searchHotels(this.text);
+  }
 
-  ngOnInit() {
+  searchHotels(text: string) {
+    this.text = text;
+
+    // chiamata GET alle REST API del mock server che ritornerà un array di Hotel[]
+    this.http.get<Hotel[]>('http://localhost:3000/hotels?q=' + text)
+      .subscribe(result => {
+        // console.log(result);
+        this.hotels = result;
+      });
   }
 
 }
