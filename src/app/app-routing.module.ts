a1cb2ea3ccb20a8c8f4ded94e6cea2b8277fe98f
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-02-15 18:24:29 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/services/auth.guard';
 import { SearchComponent } from './features/search/search.component';
 import { LoginComponent } from './features/login/login.component';
 import { CartComponent } from './features/cart/cart.component';
 
 const routes: Routes = [
-  { path: 'search', component: SearchComponent },
+  { path: 'search', component: SearchComponent  }, // canActivate: [AuthGuard]
   { path: 'login', component: LoginComponent },
-  { path: 'cart', component: CartComponent },
+  { path: 'cart', component: CartComponent }, // canActivate: [AuthGuard]
   { path: '**', redirectTo: 'search'}
 ];
 
